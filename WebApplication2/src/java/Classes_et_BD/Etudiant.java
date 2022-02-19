8e33f74cb8b1ac4352f26ff9b809da6d1ca45021
Henock <hlungelehenock@gmail.com>
2022-02-19 23:00:03 +0100

    creation des classes EEtudiant et presence

---
+++ WebApplication2/src/java/Classes_et_BD/Etudiant.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes_et_BD;
+
+/**
+ *
+ * @author Henockl
+ */
+public class Etudiant {
+    public String id_etudiant,nom_etudiant,postnom_etudiant,prenom_etudiant;
+}
---
+++ WebApplication2/src/java/Classes_et_BD/Presence.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes_et_BD;
+import java.util.Date;
+
+/**
+ *
+ * @author Henockl
+ */
+public class Presence {
+    public int id_presence;
+    public Date date = new Date();
+    public String status_presence, id_etudiant;
+}
